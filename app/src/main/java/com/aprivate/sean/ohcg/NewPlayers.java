e637a6f03f1d19f51f32037630e6f6f804d9e2c9
Sean Jones <smjones.virj@gmail.com>
2018-10-27 23:59:52 -0500

    player name validation

--- app/src/main/java/com/aprivate/sean/ohcg/NewPlayers.java
+++ app/src/main/java/com/aprivate/sean/ohcg/NewPlayers.java
@@ -2,6 +2,7 @@ package com.aprivate.sean.ohcg;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -35,18 +36,26 @@ public class NewPlayers extends AppCompatActivity {
     private void addPlayer(Button addPlayerButtonObj) {
         EditText playerNameTextBox = (EditText) findViewById(R.id.editPlayerName);
         String name = playerNameTextBox.getText().toString();
-        ScoreBoardItem newPlayer = new ScoreBoardItem();
-        newPlayer.setCurrentBid("0");
-        newPlayer.setBoardRank(0);
-        newPlayer.setCurrentPoints(0);
-        newPlayer.setPlayerName(name);
-        Global.getRecordAdapter().add(newPlayer);
-        TextView playerAddedLabelObj = (TextView) findViewById(R.id.playerAddedLabel);
-        playerAddedLabelObj.setText(name + " has been added!");
-        playerAddedLabelObj.setVisibility(View.VISIBLE);
-        playerNameTextBox.setText("");
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(playerNameTextBox.getWindowToken(), 0);
+        if(name != null && name != "" && !Global.getRecordAdapter().getScoreBoardItems().stream().anyMatch(x -> x.getPlayerName().equalsIgnoreCase(name))) {
+            ScoreBoardItem newPlayer = new ScoreBoardItem();
+            newPlayer.setCurrentBid("0");
+            newPlayer.setBoardRank(0);
+            newPlayer.setCurrentPoints(0);
+            newPlayer.setPlayerName(name);
+            Global.getRecordAdapter().add(newPlayer);
+            TextView playerAddedLabelObj = (TextView) findViewById(R.id.playerAddedLabel);
+            playerAddedLabelObj.setText(name + " has been added!");
+            playerAddedLabelObj.setVisibility(View.VISIBLE);
+            playerNameTextBox.setText("");
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(playerNameTextBox.getWindowToken(), 0);
+        }else {
+            AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("Invalid player name. Please, try again.");
+            dlgAlert.setTitle("Error");
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+        }
     }
 
 }
