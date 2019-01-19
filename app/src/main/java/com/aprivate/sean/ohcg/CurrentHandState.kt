0c34dee03dfb4a3aa211092a54e1f52eb3bae45b
Sean Jones <smjones.virj@gmail.com>
2019-01-19 12:44:55 -0600

    fix and add room to gradle

--- app/src/main/java/com/aprivate/sean/ohcg/CurrentHandState.kt
+++ app/src/main/java/com/aprivate/sean/ohcg/CurrentHandState.kt
@@ -1,16 +1,12 @@
 package com.aprivate.sean.ohcg
 
 import java.io.Serializable
-import kotlin.collections.HashMap
 
 class CurrentHandState : Serializable {
-
-
     var currentOrderNumber = 1
     var dealCount: Int = 0
 
     var totalBidsForCurrentHand: Int = 0
 
-    val playerOrderMapToIndex: HashMap<Int, ScoreBoardItem?>? = null
-
+    val playerOrderMapToIndex: MutableMap<Int, ScoreBoardItem> = mutableMapOf()
 }
